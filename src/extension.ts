b92f61ed633c340e71915ad8a0042eeeb5cf3daa
Prantik <prantiknoor@gmail.com>
2023-03-07 19:16:09 +0600

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,12 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "auto-realesed-vscode-extension" is now active!');
+
+	let disposable = vscode.commands.registerCommand('auto-realesed-vscode-extension.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World');
+	});
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
