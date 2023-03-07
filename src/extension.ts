cdc72c031e0a2b74c3a9a8da284a2b767513f4a3
Prantik <prantiknoor@gmail.com>
2023-03-07 19:18:30 +0600

    feat(extension.ts): change info message

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "auto-realesed-vscode-extension" is now active!');
 
 	let disposable = vscode.commands.registerCommand('auto-realesed-vscode-extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World');
+		vscode.window.showInformationMessage('Hello World from Prantik');
 	});
 	context.subscriptions.push(disposable);
 }
